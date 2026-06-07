be36e36b5a731d050da2b3a031ebe008ec640618
Puspita Deb Pritha <puspitapritha2000@gmail.com>
2026-06-08 00:53:42 +0600

    assignmrnt

---
+++ datatype.js
@@ -0,0 +1,11 @@
+let name = "Rahim";
+let age = 20;
+let isStudent = true;
+let salary = null;
+let phone;
+
+console.log(typeof name);      // string
+console.log(typeof age);       // number
+console.log(typeof isStudent); // boolean
+console.log(typeof salary);    // object (JavaScript bug)
+console.log(typeof phone);     // undefined
---
+++ student.js
@@ -0,0 +1,9 @@
+let name = "Puspita Deb Pritha";
+let age = 21;
+let district = "Dhaka";
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("District:", district);
+console.log("Student:", isStudent);
